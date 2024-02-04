f73c95b8e80a028af795af050b6182dd72566507
lshiyan <109442784+lshiyan@users.noreply.github.com>
2024-02-03 19:35:07 -0500

    added some arguments, created the mlp experiment file

---
+++ experiments/mlp.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn 
+
+class MLPExperiment():
+    def __init__():
+        
--- run.py
+++ run.py
@@ -4,10 +4,14 @@ import torch
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Biological deep learning')
     
-    parser.add_argument('--test', type=str, default="testing")
+    #Basic configurations.
+    parser.add_argument('--is_training', type=int, default=1, help='status')
     
     #Data loader
     parser.add_argument('--data_root', type=str, default="data/mnist")
     parser.add_argument('--data_filename', type=str, default="mnist_train.csv")
     
     args=parser.parse_args()
+    
+    if args.is_training:
+        print("training")
