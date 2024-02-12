fixes for normalization, positive weight inits

layers/hebbian_layer.py
@@ -1,27 +1,32 @@
 import torch
 import torch.nn as nn
 import math
+import matplotlib.pyplot as plt
 from numpy import outer
+
 
 #Hebbian learning layer that implements lateral inhibition in output. Not trained through supervision.
 class HebbianLayer (nn.Module):
-    def __init__(self, input_dimension, output_dimension, lamb=1, lr=0.001):
+    def __init__(self, input_dimension, output_dimension, lamb=1, heb_lr=1):
         super (HebbianLayer, self).__init__()
         self.input_dimension=input_dimension
         self.output_dimension=output_dimension
         self.lamb=lamb
-        self.lr=lr
+        self.lr=heb_lr
         
         self.fc=nn.Linear(self.input_dimension, self.output_dimension)
+  
         for param in self.fc.parameters():
+            torch.nn.init.uniform_(param, a=0.0, b=1.0)
             param.requires_grad_(False)
     
     #Calculates lateral inhibition h_mu -> (h_mu)^(lambda)/ sum on i (h_mu_i)^(lambda)
     def inhibition(self, x):
         normalization_factor=0
-        for ele in x: 
-            normalization_factor+= torch.sum(ele ** 2)
-            ele/=normalization_factor
+        normalization_factor+= torch.sum(x ** self.lamb)
+        print("Factor is:", normalization_factor)
+        x/=normalization_factor
+        print(x)
         return x
     
     #Employs hebbian learning rule, Wij->alpha*y_i*x_j. Calculates outer product of input and output and adds it to matrix.
@@ -34,11 +39,27 @@ class HebbianLayer (nn.Module):
     def forward(self, x):
         input=x
         x=self.fc(x)
+        print("After FC Layer")
+        print(x)
         x=self.inhibition(x)
         self.updateWeightsHebbian(input, x)    
+
         return x
     
-"""if __name__=="__main__":
-    hebbian_layer=HebbianLayer(3,3)
-    x=torch.tensor([1.0,2.0,3.0])
-    res=hebbian_layer(x)"""
+    #Creates heatmap of randomly chosen feature selectors.
+    def visualizeWeights(self, num_choices):
+        weight=self.fc.weight
+        random_indices = torch.randperm(self.fc.weight.size(0))[:num_choices]
+        for ele in random_indices:#Scalar tensor
+            idx=ele.item()
+            random_feature_selector=weight[idx]
+            heatmap=random_feature_selector.view(28,28)
+            plt.imshow(heatmap, cmap='hot', interpolation='nearest')
+            plt.title("HeatMap of feature selector {} with lambda {}".format(idx, self.lamb))
+            plt.colorbar()
+            plt.show()
+        return
+    
+if __name__=="__main__":
+    test_layer=HebbianLayer(3,3,1)
+    test_layer(torch.tensor([1,2,3], dtype=torch.float))

models/hebbian_network.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 
 class HebbianNetwork(nn.Module):
     
-    def __init__(self, input_dimension, hidden_layer_dimension, output_dimension, lamb=1):
+    def __init__(self, input_dimension, hidden_layer_dimension, output_dimension, heb_lr=1, lamb=1):
         super(HebbianNetwork, self).__init__()
         self.input_dimension=input_dimension
         self.output_dimension=output_dimension
         self.hidden_layer_dimension=hidden_layer_dimension
-        self.hebbian_layer=HebbianLayer(self.input_dimension, self.hidden_layer_dimension, lamb)
+        self.hebbian_layer=HebbianLayer(self.input_dimension, self.hidden_layer_dimension, lamb=lamb, heb_lr=heb_lr)
         self.classifier_layer=nn.Linear(self.hidden_layer_dimension, self.output_dimension)
         
     def forward(self,x):
@@ -17,5 +17,9 @@ class HebbianNetwork(nn.Module):
         x=self.classifier_layer(x)
         return x
     
+    def visualizeWeights(self, num_choices):
+        self.hebbian_layer.visualizeWeights(num_choices=num_choices)
+    
 if __name__=="__main__":
-    network=HebbianNetwork(3,3)
+    network=HebbianNetwork(784, 256, 10)
+    print(network)