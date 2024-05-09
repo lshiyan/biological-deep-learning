a6142e69ae403087e9d8734a49ad250aa4f1ea01
patrick-zheng <99631710+patrick-zheng@users.noreply.github.com>
2024-05-09 16:14:01 -0400

    Modified some code and added documentation

--- data/mnist_factory.py
+++ data/mnist_factory.py
@@ -1,37 +1,57 @@
 import os
 
-def convert(imgf, labelf, outf, n):
-    project_root = os.getcwd() 
-    imgf = os.path.join(project_root, imgf)
-    labelf = os.path.join(project_root, labelf)
-    outf = os.path.join(project_root, outf)
+"""
+Converts image file and labels into .csv file
+@param 
+    img_file (str) = relative path to image file
+    label_file (str) = relative path to label file
+    out_file (str) = relative path to out file
+    data_size (int) = number of data inputs to read
+"""
 
-    f = open(imgf, "rb")
-    o = open(outf, "w")
-    l = open(labelf, "rb")
+def convert(img_file, label_file, out_file, data_size):
+    # Get absolute path of all the necessary files
+    project_root = os.getcwd()
+    img_file = os.path.join(project_root, img_file)
+    label_file = os.path.join(project_root, label_file)
+    out_file = os.path.join(project_root, out_file)
+
+    # Open all necessary files
+    imgs = open(img_file, "rb")
+    out = open(out_file, "w")
+    labels = open(label_file, "rb")
     
-    f.read(16)
-    l.read(8)
+    # Skip start of file because???
+    # TODO: why skip bytes?
+    imgs.read(16)
+    labels.read(8)
+    
+    # Create a 2D list of images where each image is a 1D list where the first element is the label
+    img_size = 28*28
     images = []
 
-    for i in range(n):
-        image = [ord(l.read(1))]
-        for j in range(28*28):
-            image.append(ord(f.read(1)))
+    for i in range(data_size):
+        image = [ord(labels.read(1))]
+        for j in range(img_size):
+            image.append(ord(imgs.read(1)))
         images.append(image)
 
+    # Convert each image from 1D list to a comma seperated str and write it into out file
     for image in images:
-        o.write(",".join(str(pix) for pix in image)+"\n")
-    f.close()
-    o.close()
-    l.close()
+        out.write(",".join(str(pix) for pix in image)+"\n")
+    
+    # Close files
+    imgs.close()
+    out.close()
+    labels.close()
 
+# TODO: These lines should be written somewhere else
 convert("data/mnist/train-images.idx3-ubyte", "data/mnist/train-labels.idx1-ubyte",
         "data/mnist/mnist_train.csv", 60000)
 convert("data/mnist/t10k-images.idx3-ubyte", "data/mnist/t10k-labels.idx1-ubyte",
         "data/mnist/mnist_test.csv", 10000)
 
-convert("data/fashion_mnist/train-images-idx3-ubyte", "data/fashion_mnist/train-labels-idx1-ubyte",
+convert("data/fashion_mnist/train-images.idx3-ubyte", "data/fashion_mnist/train-labels.idx1-ubyte",
         "data/fashion_mnist/fashion-mnist_train.csv", 60000)
-convert("data/fashion_mnist/t10k-images-idx3-ubyte", "data/fashion_mnist/t10k-labels-idx1-ubyte",
+convert("data/fashion_mnist/t10k-images.idx3-ubyte", "data/fashion_mnist/t10k-labels.idx1-ubyte",
         "data/fashion_mnist/fashion-mnist_test.csv", 10000)
--- run.py
+++ run.py
@@ -5,12 +5,12 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Biological deep learning')
     
     #Basic configurations.
-    parser.add_argument('--is_training', type=int, default=1, help='status')
+    parser.add_argument('--is_training', type=bool, default=True, help='status')
     
     #Data loader.
-    parser.add_argument('--data_root', type=str, default="mnist/")
-    parser.add_argument('--data_filename', type=str, default="mnist_train.csv")
-    
+    parser.add_argument('--data_root', type=str, default="data/mnist/")
+    parser.add_argument('--train_data_filename', type=str, default="mnist_train.csv")
+    parser.add_argument('--test_data_filename', type=str, default="mnist_test.csv")
     
     #Hyperparameters.
     parser.add_argument('--learning_rate', type=int, default=0.001)
