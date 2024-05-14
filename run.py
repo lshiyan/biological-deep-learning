d6233564af98e081f09b94d81380785db508dd12
patrick-zheng <99631710+patrick-zheng@users.noreply.github.com>
2024-05-14 10:23:56 -0400

    Added more arguments to parser in run.py

--- run.py
+++ run.py
@@ -4,15 +4,21 @@ import torch
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Biological deep learning')
     
-    #Basic configurations.
+    # Basic configurations.
     parser.add_argument('--is_training', type=bool, default=True, help='status')
     
-    #Data loader.
+    # Data Factory
+    parser.add_argument('--test_data', type=str, default="data/mnist/t10k-images.idx3-ubyte")
+    parser.add_argument('--test_labels', type=str, default="data/mnist/t10k-labels.idx1-ubyte")
+    parser.add_argument('--train_data', type=str, default="data/mnist/train-images.idx3-ubyte")
+    parser.add_argument('--train_labels', type=str, default="data/mnist/train-labels.idx1-ubyte")
+    
+    # Data loader.
     parser.add_argument('--data_root', type=str, default="data/mnist/")
     parser.add_argument('--train_data_filename', type=str, default="mnist_train.csv")
     parser.add_argument('--test_data_filename', type=str, default="mnist_test.csv")
     
-    #Hyperparameters.
+    # Hyperparameters.
     parser.add_argument('--learning_rate', type=int, default=0.001)
     parser.add_argument('--num_epochs', type=int, default=100)
     
