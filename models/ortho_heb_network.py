865e52f513f43d74b9d035174b2b23786cbb42d8
patrick-zheng <99631710+patrick-zheng@users.noreply.github.com>
2024-06-26 12:07:18 -0400

    trying out fully orthogonal learning rule

---
+++ models/ortho_heb_network.py
@@ -0,0 +1,87 @@
+import argparse
+import torch
+from layers.input_layer import InputLayer
+from layers.ortho_heb_layer import OrthoHebLayer
+from layers.classifier_layer import ClassifierLayer
+from layers.layer import NetworkLayer
+from models.network import Network 
+
+
+class OrthoHebNetwork(Network):
+    """
+    CLASS
+    Defining the base hebbian network
+    @instance attr.
+        PARENT ATTR.
+            device_id (int) = id of the gpu that the model will be running in
+        OWN ATTR.
+            input_dim (int) = number of inputs
+            heb_dim (int) = number of neurons in hebbina layer
+            output_dimension (int) = number of output neurons
+            heb_param (dict {str:float}) = dictionary with all the hyperparameters for the hebbian layer
+                - lr (float) = learning rate of hebbian layer
+                - lamb (float) = hyperparameter for lateral neuron inhibition
+                - gam (float) = factor to decay learning rate of hebbian layer
+                - eps (float) = small value to avoid 0 division
+            cla_param (dict {str:float}) = dictionary with all the hyperparameters for the classification layer
+            lr (float) = learning rate of classification layer
+    """
+    def __init__(self, args: argparse.Namespace) -> None:
+        """
+        CONSTRUCTOR METHOD
+        @pram
+            args: arguments passed from command line
+        @return
+            None
+        """
+        super().__init__(args.device_id)
+
+        # Dimension of each layer
+        self.input_dim: int = args.input_dim
+        self.heb_dim: int = args.heb_dim
+        self.output_dim: int = args.output_dim
+
+        # Hebbian layer hyperparameters stored in dictionary
+        self.heb_param: dict[str:float] = {
+            "lamb": args.heb_lamb,
+            "eps": args.heb_eps,
+            "gam": args.heb_gam
+        }
+
+        # Classification layer hyperparameters stored in dictionary
+        self.cla_param: dict[:] = {}
+
+        # Shared hyperparameters
+        self.lr: float = args.lr
+
+        # Setting up layers of the network
+        input_layer: NetworkLayer = InputLayer(args.train_data, args.train_label, args.train_filename, args.test_data, args.test_label, args.test_filename)
+        hebbian_layer: NetworkLayer = OrthoHebLayer(self.input_dim, self.heb_dim, self.device_id, self.heb_param["lamb"], self.lr, self.heb_param["gam"], self.heb_param["eps"])
+        classification_layer: NetworkLayer = ClassifierLayer(self.heb_dim, self.output_dim, self.device_id, self.lr)
+        
+        self.add_module("Input Layer", input_layer)
+        self.add_module("Hebbian Layer", hebbian_layer)
+        self.add_module("Classification Layer", classification_layer)
+
+
+    def forward(self, input: torch.Tensor, clamped_output: torch.Tensor = None) -> torch.Tensor:
+        """
+        METHOD
+        Defines how an input data flows throw the network
+        @param
+            input: input data into the network
+            clamped_output: one-hot encode of true labels
+        @return
+            output: returns the data after passing it throw the network
+        """
+        hebbian_layer = self.get_module("Hebbian Layer")
+        classification_layer = self.get_module("Classification Layer")
+
+        if input.dtype != torch.float32:
+            input = input.float().to(self.device_id)
+
+        data_input = input.to(self.device_id)
+        post_hebbian_value = hebbian_layer(data_input)
+        output = classification_layer(post_hebbian_value, clamped_output)
+
+        return output
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-import sys
 from experiments.base_heb_cpu import BaseHebCPU
 from models.hebbian_network import HebbianNetwork
+from models.ortho_heb_network import OrthoHebNetwork
 from utils.experiment_parser import *
 from utils.experiment_comparer import *
 from utils.experiment_logger import *
@@ -15,7 +15,7 @@ ARGS = parse_arguments()
 
 # Experiments setup
 # lambda_test = [1, 5, 10, 15]
-lambda_test = [1]
+lambda_test = [15]
 
 # for num in range(1,16):
 #     lambda_test.append(1/num)
@@ -36,16 +36,23 @@ for l in lambda_test:
     #     train_acc_list.append(train_acc)
     #     experiment.cleanup()
     
-    model = HebbianNetwork(ARGS)
-    experiment = BaseHebCPU(model, ARGS, f'cpu-{l}')
-    test_acc, train_acc = experiment.run()
-
-    experiment.cleanup()
+    base_model = HebbianNetwork(ARGS)
+    base_experiment = BaseHebCPU(base_model, ARGS, f'cpu-base-{l}-')
+    base_test_acc, base_train_acc = base_experiment.run()
+    base_experiment.cleanup()
+    results_log.info(f"Epoch: {ARGS.epochs} || Lambda: {l} || Test Acc: {base_test_acc} || Train Acc: {base_train_acc}")
+    
+    ortho_model = OrthoHebNetwork(ARGS)
+    ortho_exp = BaseHebCPU(ortho_model, ARGS, f'cpu-ortho-{l}')
+    ortho_test_acc, ortho_train_acc = ortho_exp.run()
+    ortho_exp.cleanup()
+    results_log.info(f"Epoch: {ARGS.epochs} || Lambda: {l} || Test Acc: {ortho_test_acc} || Train Acc: {ortho_train_acc}")
+    
     
     # avg_test = average(test_acc_list)
     # var_test = variance(test_acc_list)
     # avg_train = average(train_acc_list)
     # var_train = variance(train_acc_list)
     
-    results_log.info(f"Epoch: {ARGS.epochs} || Lambda: {l} || Test Acc: {test_acc} || Train Acc: {train_acc}")
+    
     # results_log.info(f"Epoch: {ARGS.epochs} || Lambda: {l} || Test Acc: avg = {avg_test}, var = {var_test} || Train Acc: avg = {avg_train}, var = {var_train}")
