c46a47239ff168153ad1403519559c44d25fb6a2
XuTiany1 <tiany12138@gmail.com>
2024-07-18 12:48:57 -0400

    added timer to all, fixed testing dataloader logic

--- experiments/forget_experiment.py
+++ experiments/forget_experiment.py
@@ -129,6 +129,8 @@ class ForgetExperiment(Experiment):
             curr_test_dataloader: DataLoader = self.sub_experiemnts_test_dataloader_list[step]
             self.curr_folder_path: str = self.RESULT_PATH + f"{self.data_name}_{'_'.join(map(str, self.sub_experiment_scope_list[step]))}"
 
+            self.testing_test_dataloader_list.append(curr_test_dataloader)
+
             for epoch in range(self.epochs):
 
                 self._training(curr_train_dataloader, epoch, self.data_name, ExperimentPhases.FORGET)
@@ -207,8 +209,20 @@ class ForgetExperiment(Experiment):
                  visualize: bool = True,
                  ) -> Union[float, Tuple[float, ...]]:
         
+        test_start: float = time.time()
+        self.EXP_LOG.info(f"Started '_testing' function with {dname.upper()}.")
+
         sub_experiment_name = self.curr_folder_path.split('/')[-1]  # Assumes '/' as the path separator.
         
+        # Epoch and batch set up
+        test_batches_per_epoch = len(test_data_loader)
+        self.EXP_LOG.info(f"Sub-experiemnt to be tested is {sub_experiment_name} -- Number of current experiment samples seen is {self.SUB_EXP_SAMPLES} -- Number of total experiment samples seen is {self.TOTAL_SAMPLES}")
+        self.EXP_LOG.info(f"This testing is with {test_batches_per_epoch} batches of size {self.batch_size} in this epoch.")
+        
+        # Set the model to evaluation mode - important for layers with different training / inference behaviour
+        self.model.eval()
+        self.EXP_LOG.info("Set the model to testing mode.")
+
         final_accuracy: float = 0
 
         with torch.no_grad():
@@ -230,6 +244,7 @@ class ForgetExperiment(Experiment):
             final_accuracy = correct_test_count/total_test_count
                 
         test_end = time.time()
+        testing_time = test_end - test_start
 
         if purpose == Purposes.TEST_ACCURACY: 
             self.TEST_LOG.info(f'Current Experiment: {sub_experiment_name} || Current Subexperiment Samples Seen: {self.SUB_EXP_SAMPLES} || Total Samples Seen: {self.TOTAL_SAMPLES} || Test Accuracy: {final_accuracy}')
@@ -237,6 +252,10 @@ class ForgetExperiment(Experiment):
         if purpose == Purposes.TRAIN_ACCURACY: 
             self.TRAIN_LOG.info(f'Current Experiment: {sub_experiment_name} || Current Subexperiment Samples Seen: {self.SUB_EXP_SAMPLES} || Total Samples Seen: {self.TOTAL_SAMPLES} || Train Accuracy: {final_accuracy}')
         
+        self.EXP_LOG.info(f"Completed testing with {correct_test_count} out of {total_test_count}.")
+        self.EXP_LOG.info("Completed '_testing' function.")
+        self.EXP_LOG.info(f"Testing ({purpose.value.lower()} acc) of sample #{self.SAMPLES} took {time_to_str(testing_time)}.")
+
         if visualize: 
             self.model.visualize_weights(self.curr_folder_path, self.SAMPLES, purpose.name.lower())
 
@@ -279,29 +298,83 @@ class ForgetExperiment(Experiment):
 
         full_list_of_accuracy = list_of_test_accuracy + list_of_train_accuracy
         return full_list_of_accuracy
-"""
-Concatenating the lists
-    So, the entries will be the order as follows:
-    TEST ACCURACY SECTION
-        digits 0 and 1 test accuracy
-        digits 2 and 3 test accuracy
-        digits 4 and 5 test accuracy
-        digits 6 and 7 test accuracy
-        digits 8 and 9 test accuracy
-    TRAIN ACCURACY SECTION
-        digits 0 and 1 train accuracy
-        digits 2 and 3 train accuracy
-        digits 4 and 5 train accuracy
-        digits 6 and 7 train accuracy
-        digits 8 and 9 train accuracy
-"""
 
+#Concatenating the lists
+#    So, the entries will be the order as follows:
+#    TEST ACCURACY SECTION
+#        digits 0 and 1 test accuracy
+#        digits 2 and 3 test accuracy
+#        digits 4 and 5 test accuracy
+#        digits 6 and 7 test accuracy
+#        digits 8 and 9 test accuracy
+#    TRAIN ACCURACY SECTION
+#        digits 0 and 1 train accuracy
+#        digits 2 and 3 train accuracy
+#        digits 4 and 5 train accuracy
+#        digits 6 and 7 train accuracy
+#        digits 8 and 9 train accuracy
+
+
+    def _param_start_log(self):
+        self.EXP_LOG.info("Started logging of experiment parameters.")
         
+        self.PARAM_LOG.info(f"Experiment Type: {self.experiment_type.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Device: {self.device.upper()}")
+        self.PARAM_LOG.info(f"Input Dimension: {self.model.input_dim}")
+        self.PARAM_LOG.info(f"Hebbian Layer Dimension: {self.model.heb_dim}")
+        self.PARAM_LOG.info(f"Outout Dimension: {self.model.output_dim}")
+        self.PARAM_LOG.info(f"Hebbian Layer Lambda: {self.model.heb_lamb}")
+        self.PARAM_LOG.info(f"Hebbian Layer Gamma: {self.model.heb_gam}")
+        self.PARAM_LOG.info(f"Hebbian Layer Epsilon: {self.model.heb_eps}")
+        self.PARAM_LOG.info(f"Hebbian Layer Sigmoid K: {self.model.sig_k}")
+        self.PARAM_LOG.info(f"Learning Rule: {self.model.learn.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Inhibition Rule: {self.model.inhib.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Weight Growth: {self.model.growth.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Weight Decay: {self.model.weight_decay.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Bias Update: {self.model.bias_update.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Network Learning Rate: {self.model.lr}")
+        self.PARAM_LOG.info(f"Alpha: {self.model.alpha}")
+        self.PARAM_LOG.info(f"Beta: {self.model.beta}")
+        self.PARAM_LOG.info(f"Sigma: {self.model.sigma}")
+        self.PARAM_LOG.info(f"Mu: {self.model.mu}")
+        self.PARAM_LOG.info(f"Param Init: {self.model.init.value.lower().capitalize()}")
+        self.PARAM_LOG.info(f"Sub experiment scope list: {self.sub_experiment_scope_list}")
+        self.PARAM_LOG.info(f"Start time of experiment: {time.strftime('%Y-%m-%d %Hh:%Mm:%Ss', time.localtime(self.START_TIME))}")
+        
+        self.EXP_LOG.info("Completed logging of experiment parameters.")
+
+#TODO need to fix this to be more accurate and represent the training and testing time of each individual sub experiment
+    def _param_end_log(self):
+        self.PARAM_LOG.info(f"End time of experiment: {time.strftime('%Y-%m-%d %Hh:%Mm:%Ss', time.localtime(self.END_TIME))}")
+        self.PARAM_LOG.info(f"Runtime of experiment: {time_to_str(self.DURATION if self.DURATION != None else 0)}")
+        self.PARAM_LOG.info(f"Total train time of experiment: {time_to_str(self.TRAIN_TIME)}")
+        self.PARAM_LOG.info(f"Total test time (test acc) of experiment: {time_to_str(self.TEST_ACC_TIME)}")
+        self.PARAM_LOG.info(f"Total test time (train acc) of experiment: {time_to_str(self.TRAIN_ACC_TIME)}")
     
+    
+    def _final_test_log(self, results) -> None:
 
+        test_acc_digit_0_1 = results[0]
+        test_acc_digit_2_3 = results[1]
+        test_acc_digit_4_5 = results[2]
+        test_acc_digit_6_7 = results[3]
+        test_acc_digit_8_9 = results[4]
+        train_acc_digit_0_1 = results[5]
+        train_acc_digit_2_3 = results[6]
+        train_acc_digit_4_5 = results[7]
+        train_acc_digit_6_7 = results[8]
+        train_acc_digit_8_9 = results[9]
 
-
-
+        self.PARAM_LOG.info(f"Testing accuracy of model on digits 0 and 1 after training for {self.epochs} epochs: {test_acc_digit_0_1}")
+        self.PARAM_LOG.info(f"Testing accuracy of model on digits 2 and 3 after training for {self.epochs} epochs: {test_acc_digit_2_3}")
+        self.PARAM_LOG.info(f"Testing accuracy of model on digits 4 and 5 after training for {self.epochs} epochs: {test_acc_digit_4_5}")
+        self.PARAM_LOG.info(f"Testing accuracy of model on digits 6 and 7 after training for {self.epochs} epochs: {test_acc_digit_6_7}")
+        self.PARAM_LOG.info(f"Testing accuracy of model on digits 8 and 9 after training for {self.epochs} epochs: {test_acc_digit_8_9}")
+        self.PARAM_LOG.info(f"Training accuracy of model on digits 0 and 1 after training for {self.epochs} epochs: {train_acc_digit_0_1}")
+        self.PARAM_LOG.info(f"Training accuracy of model on digits 2 and 3 after training for {self.epochs} epochs: {train_acc_digit_2_3}")
+        self.PARAM_LOG.info(f"Training accuracy of model on digits 4 and 5 after training for {self.epochs} epochs: {train_acc_digit_4_5}")
+        self.PARAM_LOG.info(f"Training accuracy of model on digits 6 and 7 after training for {self.epochs} epochs: {train_acc_digit_6_7}")
+        self.PARAM_LOG.info(f"Training accuracy of model on digits 8 and 8 after training for {self.epochs} epochs: {train_acc_digit_8_9}")
 
 
 
