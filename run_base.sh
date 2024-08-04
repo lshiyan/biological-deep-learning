e43001890bb4541652226820081eecc729626ed0
patrick-zheng <99631710+patrick-zheng@users.noreply.github.com>
2024-08-03 22:27:01 -0400

    better bash script

--- run_base.sh
+++ run_base.sh
@@ -9,10 +9,10 @@ scriptName='train_base.py'
 # heb_growth_values=("linear" "sigmoid" "exponential")
 # heb_focus_values=("synapse" "neuron")
 
-lambda_values=(16)
-heb_learn_values=('orthogonal')
-heb_growth_values=('linear')
-heb_focus_values=('synapse')
+lambda_values=(0.25 1 16)
+heb_learn_values=('sanger' 'orthogonal')
+heb_growth_values=('linear' 'sigmoid' 'exponential')
+heb_focus_values=('synapse' 'neuron')
 
 # Process List
 processes=()
