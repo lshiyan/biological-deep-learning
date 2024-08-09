8cffb763b8033dc6f566ff4ca28e59b3631d0f84
Tianyi Xu <76981871+XuTiany1@users.noreply.github.com>
2024-08-09 07:29:04 -0400

    learning rate testing

--- run_base.sh
+++ run_base.sh
@@ -9,10 +9,12 @@ scriptName='train_base.py'
 # heb_growth_values=("linear" "sigmoid" "exponential")
 # heb_focus_values=("synapse" "neuron")
 
-lambda_values=(0.25 1 16)
+lambda_values=(0.25 0.5 1 2 4 8 16 32 64)
 heb_learn_values=('sanger' 'orthogonal')
 heb_growth_values=('linear' 'sigmoid' 'exponential')
+clas_growth_values=('linear' 'sigmoid')
 heb_focus_values=('synapse' 'neuron')
+learning_rate_values=(0.1 0.5 0.01 0.05 0.001 0.005 0.0001 0.0005)
 
 # Process List
 processes=()
@@ -22,52 +24,57 @@ for focus in "${heb_focus_values[@]}"; do
     for heb_growth in "${heb_growth_values[@]}"; do
         for heb_learn in "${heb_learn_values[@]}"; do
             for lambda in "${lambda_values[@]}"; do
-                # Construct the complete set of arguments including the varying parameter
-                arguments=(
-                    "--data_name=MNIST"
-                    "--train_data=data/mnist/train-images.idx3-ubyte"
-                    "--train_label=data/mnist/train-labels.idx1-ubyte"
-                    "--test_data=data/mnist/test-images.idx3-ubyte"
-                    "--test_label=data/mnist/test-labels.idx1-ubyte"
-                    "--train_size=60000"
-                    "--test_size=10000"
-                    "--classes=10"
-                    "--train_fname=data/mnist/mnist_train.csv"
-                    "--test_fname=data/mnist/mnist_test.csv"
-                    "--input_dim=784"
-                    "--heb_dim=64"
-                    "--output_dim=10"
-                    "--heb_gam=0.99"
-                    "--heb_eps=0.0001"
-                    "--heb_inhib=relu"
-                    "--heb_focus=$focus"
-                    "--heb_growth=$heb_growth"
-                    "--heb_learn=$heb_learn"
-                    "--heb_lamb=$lambda"
-                    "--class_learn=hebbian"
-                    "--class_growth=linear"
-                    "--class_bias=no_bias"
-                    "--class_focus=synapse"
-                    "--class_act=basic"
-                    "--lr=0.005"
-                    "--sigmoid_k=1"
-                    "--alpha=0"
-                    "--beta=10e-7"
-                    "--sigma=1"
-                    "--mu=0"
-                    "--init=uniform"
-                    "--batch_size=1"
-                    "--epochs=1"
-                    "--device=cpu"
-                    "--local_machine=True"
-                    "--experiment_type=base"
-                )
-                
-                # Start the process with the complete set of arguments
-                allArgs=($scriptName "${arguments[@]}")
-                python "${allArgs[@]}" &
-                processes+=($!)
-                echo "Started process: ${processes[-1]}"
+                for lr in "${learning_rate_values[@]}"; do
+                    for clas_growth in "${clas_growth_values[@]}"; do
+                        # Construct the complete set of arguments including the varying parameter
+                        arguments=(
+                            "--data_name=MNIST"
+                            "--train_data=data/mnist/train-images.idx3-ubyte"
+                            "--train_label=data/mnist/train-labels.idx1-ubyte"
+                            "--test_data=data/mnist/test-images.idx3-ubyte"
+                            "--test_label=data/mnist/test-labels.idx1-ubyte"
+                            "--train_size=60000"
+                            "--test_size=10000"
+                            "--classes=10"
+                            "--train_fname=data/mnist/mnist_train.csv"
+                            "--test_fname=data/mnist/mnist_test.csv"
+                            "--input_dim=784"
+                            "--heb_dim=64"
+                            "--output_dim=10"
+                            "--heb_gam=0.99"
+                            "--heb_eps=0.0001"
+                            "--heb_inhib=relu"
+                            "--heb_focus=$focus"
+                            "--heb_growth=$heb_growth"
+                            "--heb_learn=$heb_learn"
+                            "--heb_lamb=$lambda"
+                            "--heb_act=normalized"
+                            "--class_learn=hebbian"
+                            "--class_growth=$clas_growth"
+                            "--class_bias=no_bias"
+                            "--class_focus=neuron"
+                            "--class_act=normalized"
+                            "--lr=$lr"
+                            "--sigmoid_k=1"
+                            "--alpha=0"
+                            "--beta=1e-2"
+                            "--sigma=1"
+                            "--mu=0"
+                            "--init=uniform"
+                            "--batch_size=1"
+                            "--epochs=1"
+                            "--device=cpu"
+                            "--local_machine=True"
+                            "--experiment_type=base"
+                        )
+                        
+                        # Start the process with the complete set of arguments
+                        allArgs=($scriptName "${arguments[@]}")
+                        python "${allArgs[@]}" &
+                        processes+=($!)
+                        echo "Started process: ${processes[-1]}"
+                    done
+                done
             done
         done
     done
