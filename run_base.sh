5bfa213496a9dbd715fa0eaae66498e119a3f307
Tianyi Xu <76981871+XuTiany1@users.noreply.github.com>
2024-08-10 06:59:02 -0400

    Update run_base.sh

--- run_base.sh
+++ run_base.sh
@@ -14,6 +14,7 @@ heb_learn_values=('sanger' 'orthogonal' 'HEBBIAN')
 heb_growth_values=('linear' 'sigmoid' 'exponential')
 clas_growth_values=('linear' 'sigmoid')
 heb_focus_values=('synapse' 'neuron')
+heb_inhib_values=('RELU' 'EXP_SOFTMAX')
 learning_rate_values=(0.01 0.05 0.001 0.005 0.0001 0.0005 0.00001 0.00005)
 
 # Process List
@@ -26,55 +27,57 @@ for focus in "${heb_focus_values[@]}"; do
             for lambda in "${lambda_values[@]}"; do
                 for lr in "${learning_rate_values[@]}"; do
                     for clas_growth in "${clas_growth_values[@]}"; do
-                        # Construct the complete set of arguments including the varying parameter
-                        arguments=(
-                            "--data_name=MNIST"
-                            "--experiment_name=_LR_EXPLORATION_"
-                            "--train_data=data/mnist/train-images.idx3-ubyte"
-                            "--train_label=data/mnist/train-labels.idx1-ubyte"
-                            "--test_data=data/mnist/test-images.idx3-ubyte"
-                            "--test_label=data/mnist/test-labels.idx1-ubyte"
-                            "--train_size=60000"
-                            "--test_size=10000"
-                            "--classes=10"
-                            "--train_fname=data/mnist/mnist_train.csv"
-                            "--test_fname=data/mnist/mnist_test.csv"
-                            "--input_dim=784"
-                            "--heb_dim=64"
-                            "--output_dim=10"
-                            "--heb_gam=0.99"
-                            "--heb_eps=0.0001"
-                            "--heb_inhib=relu"
-                            "--heb_focus=$focus"
-                            "--heb_growth=$heb_growth"
-                            "--heb_learn=$heb_learn"
-                            "--heb_lamb=$lambda"
-                            "--heb_act=normalized"
-                            "--class_learn=hebbian"
-                            "--class_growth=$clas_growth"
-                            "--class_bias=no_bias"
-                            "--class_focus=neuron"
-                            "--class_act=normalized"
-                            "--lr=$lr"
-                            "--sigmoid_k=1"
-                            "--alpha=0"
-                            "--beta=1e-2"
-                            "--sigma=1"
-                            "--mu=0"
-                            "--init=uniform"
-                            "--batch_size=1"
-                            "--epochs=1"
-                            #"--device=cpu"
-                            "--device=cuda:4"
-                            "--local_machine=True"
-                            "--experiment_type=base"
-                        )
-                        
-                        # Start the process with the complete set of arguments
-                        allArgs=($scriptName "${arguments[@]}")
-                        python "${allArgs[@]}" &
-                        processes+=($!)
-                        echo "Started process: ${processes[-1]}"
+                        for heb_inhib in "${heb_inhib_values[@]}"; do
+                            # Construct the complete set of arguments including the varying parameter
+                            arguments=(
+                                "--data_name=MNIST"
+                                "--experiment_name=_LR_EXPLORATION_"
+                                "--train_data=data/mnist/train-images.idx3-ubyte"
+                                "--train_label=data/mnist/train-labels.idx1-ubyte"
+                                "--test_data=data/mnist/test-images.idx3-ubyte"
+                                "--test_label=data/mnist/test-labels.idx1-ubyte"
+                                "--train_size=60000"
+                                "--test_size=10000"
+                                "--classes=10"
+                                "--train_fname=data/mnist/mnist_train.csv"
+                                "--test_fname=data/mnist/mnist_test.csv"
+                                "--input_dim=784"
+                                "--heb_dim=64"
+                                "--output_dim=10"
+                                "--heb_gam=0.99"
+                                "--heb_eps=0.0001"
+                                "--heb_inhib=$heb_inhib"
+                                "--heb_focus=$focus"
+                                "--heb_growth=$heb_growth"
+                                "--heb_learn=$heb_learn"
+                                "--heb_lamb=$lambda"
+                                "--heb_act=normalized"
+                                "--class_learn=hebbian"
+                                "--class_growth=$clas_growth"
+                                "--class_bias=no_bias"
+                                "--class_focus=neuron"
+                                "--class_act=normalized"
+                                "--lr=$lr"
+                                "--sigmoid_k=1"
+                                "--alpha=0"
+                                "--beta=1e-2"
+                                "--sigma=1"
+                                "--mu=0"
+                                "--init=uniform"
+                                "--batch_size=1"
+                                "--epochs=1"
+                                #"--device=cpu"
+                                "--device=cuda:4"
+                                "--local_machine=True"
+                                "--experiment_type=base"
+                            )
+                            
+                            # Start the process with the complete set of arguments
+                            allArgs=($scriptName "${arguments[@]}")
+                            python "${allArgs[@]}" &
+                            processes+=($!)
+                            echo "Started process: ${processes[-1]}"
+                        done
                     done
                 done
             done
