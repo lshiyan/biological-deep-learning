d149623d2975f32ff9de59c071f7f7c8651c69ec
Isabeau <isabeau.premont.schwarz@cryptolab.net>
2024-09-20 04:42:54 -0400

    Add ConvSoftHebbLayer which just uses unfold and SoftHebbLayer

--- models/hyperparams.py
+++ models/hyperparams.py
@@ -33,3 +33,12 @@ def oneHotEncode(labels, num_classes, device):
     one_hot_encoded = torch.zeros(len(labels), num_classes).to(device)
     one_hot_encoded.scatter_(1, labels.unsqueeze(1), 1)
     return one_hot_encoded
+
+
+def cnn_output_formula_1D(size, kernel, padding, dilation, stride):
+    return int((size + 2 * padding - dilation * (kernel -1) - 1)/stride + 1)
+
+def cnn_output_formula_2D(shape, kernel, padding, dilation, stride):
+    h = cnn_output_formula_1D(shape[0],  kernel, padding, dilation, stride)
+    w = cnn_output_formula_2D(shape[0],  kernel, padding, dilation, stride)
+    return h, w
