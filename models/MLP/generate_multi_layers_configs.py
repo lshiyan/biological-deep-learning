6fe945983b53d06ad0d831107990b3168a4bc572
aikido-sensei <88054028+aikido-sensei@users.noreply.github.com>
2024-11-20 14:23:26 -0500

    Added tests for multi layers mlp

---
+++ models/MLP/generate_multi_layers_configs.py
@@ -0,0 +1,40 @@
+import json
+import os
+import itertools
+
+def generate_mlp_config_files(hsizes, lambds, wlrs, blrs, llrs, w_norm):
+    allc = list(itertools.product(hsizes, lambds, wlrs, blrs, llrs, w_norm))
+    rank = 0
+    for comb in allc:
+        config = {
+            "hsize" : comb[0],
+            "lambd" : comb[1],
+            "w_lr" : comb[2],
+            "b_lr" : comb[3],
+            "l_lr" : comb[4], 
+            "w_norm":comb[5],
+            "num_layers": 1
+        }
+        with open("ConfigsMLP/config" + str(rank) + ".json", "w") as jfile:
+            json.dump(config, jfile, indent=4)
+
+
+def generate_mlp_config_files():
+
+    w = [0.001, 0.0033, 0.01, 0.1, 0.3, 0.5]
+    for j in range(len(w)):
+        for i in range(0, 10):
+            config = {
+            "hsize": 2048,
+            "lambd": 250, 
+            "w_lr": 0.3,
+            "b_lr": 0.0033,
+            "l_lr": 0.1,
+            "w_norm": 0.01,
+            "num_layers": 1
+            }
+            with open("ConfigsMLP/config" + str(i + j*10) + ".json", "w") as jfile:
+                json.dump(config, jfile, indent=4)
+
+
+generate_mlp_config_files()
