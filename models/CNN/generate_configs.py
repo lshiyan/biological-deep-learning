804de3b0d821ea18cd437abf016d2ed1c934f726
Genevieve Larose-de Billy <genevieve.larose-debilly@mail.mcgill.ca>
2025-01-22 16:21:44 +0000

    New Hyperparameter search for CNN

--- models/CNN/generate_configs.py
+++ models/CNN/generate_configs.py
@@ -1,8 +1,8 @@
 import json
 import os
 
-def generate_cnn_config_files(base_config, output_dir="ConfigsCNN", num_layers=[1, 1, 1, 1, 1], whiten_values=[True], triangle_values=[True],
-    greedytrain_values=[True], inhibition_values=['Softmax'], pooling_values = ['PoolingStride1']):
+def generate_cnn_config_files(base_config, output_dir="ConfigsCNN", num_layers=[1], whiten_values=[True], triangle_values=[True],
+    greedytrain_values=[True], inhibition_values=['RePU'], pooling_values = ['PoolingStride1'], hsize_values = [4000, 7000, 10000]):
     
     # whiten = False for now
     # inhibition_values = REPU for now
@@ -10,62 +10,66 @@ def generate_cnn_config_files(base_config, output_dir="ConfigsCNN", num_layers=[
     os.makedirs(output_dir, exist_ok=True)
     config_number = 0
 
-    for greedytrain in greedytrain_values:
-        for layers in num_layers:
-            for whiten in whiten_values:
-                for triangle in triangle_values:
-                    for inhibition in inhibition_values:
-                        for pool in pooling_values:
-                            
-                                config = json.loads(json.dumps(base_config))
-                                config["Lambda"]= 125
-                                config["classifierLr"]= 6e-4
-                                config["w_norm"]= 0.0001
-                                config["w_lr"]= 0.1
-                                config["l_lr"]= 0.3
-                                config["b_lr"]= 0.001
-                                config['greedytrain'] = greedytrain
-                                config['nConvLayers'] = layers
+    for hsize in hsize_values:
 
-                                config['Convolutions']["GlobalParams"]['inhibition'] = inhibition
-                                config['Convolutions']["GlobalParams"]['padding'] = 2
-                                config['Convolutions']["GlobalParams"]['paddingmode'] = "reflect"
-                                config['Convolutions']["GlobalParams"]['triangle'] = triangle
-                                config['Convolutions']["GlobalParams"]['whiten'] = whiten
-                                config['Convolutions']["GlobalParams"]['batchnorm'] = True
-
-                                if pool == 'PoolingStride1':
-                                    config['PoolingBlock']["GlobalParams"]['Pooling'] = True
-                                else:
-                                    config['PoolingBlock']["GlobalParams"]['Pooling'] = False
-
-                                if pool == 'NoPoolingStride2':
-                                    config['Convolutions']["GlobalParams"]['stride'] = 2
-                                else:
-                                    config['Convolutions']["GlobalParams"]['stride'] = 1
-
-                                # Remove convolution layers beyond the specified number
-                                for i in range(layers + 1, 5):
-                                    config['Convolutions']['Layers'].pop(f"Conv{i}", None)
-
+        for greedytrain in greedytrain_values:
+            for layers in num_layers:
+                for whiten in whiten_values:
+                    for triangle in triangle_values:
+                        for inhibition in inhibition_values:
+                            for pool in pooling_values:
                                 
-                                if config['PoolingBlock']["GlobalParams"]['Pooling'] == True:
-                                    config['PoolingBlock']['Layers'][f"Conv{layers}"]['Type'] = "Avg"
+                                    config = json.loads(json.dumps(base_config))
+                                    config["Lambda"]= 500
+                                    config["classifierLr"]= 0.001
+                                    config["w_norm"]= 0.0001
+                                    config["w_lr"]= 0.1
+                                    config["l_lr"]= 0.3
+                                    config["b_lr"]= 0.001
+                                    config['greedytrain'] = greedytrain
+                                    config['nConvLayers'] = layers
+
+                                    config['Convolutions']["GlobalParams"]['inhibition'] = inhibition
+                                    config['Convolutions']["GlobalParams"]['padding'] = 2
+                                    config['Convolutions']["GlobalParams"]['paddingmode'] = "reflect"
+                                    config['Convolutions']["GlobalParams"]['triangle'] = triangle
+                                    config['Convolutions']["GlobalParams"]['whiten'] = whiten
+                                    config['Convolutions']["GlobalParams"]['batchnorm'] = True
+
+                                    if pool == 'PoolingStride1':
+                                        config['PoolingBlock']["GlobalParams"]['Pooling'] = True
+                                    else:
+                                        config['PoolingBlock']["GlobalParams"]['Pooling'] = False
+
+                                    if pool == 'NoPoolingStride2':
+                                        config['Convolutions']["GlobalParams"]['stride'] = 2
+                                    else:
+                                        config['Convolutions']["GlobalParams"]['stride'] = 1
+
+                                    config['Convolutions']['Layers']['Conv1']['out_channel'] = hsize
+
+                                    # Remove convolution layers beyond the specified number
                                     for i in range(layers + 1, 5):
-                                        config['PoolingBlock']['Layers'].pop(f"Conv{i}", None)
-                                else:
-                                    for i in range(0, 5):
-                                        config['PoolingBlock']['Layers'].pop(f"Conv{i}", None)
+                                        config['Convolutions']['Layers'].pop(f"Conv{i}", None)
+
                                     
-                                
-                                filename = f"config{config_number}.json"
-                                filepath = os.path.join(output_dir, filename)
-                                
-                                with open(filepath, 'w') as json_file:
-                                    json.dump(config, json_file, indent=4)
-                                
-                                print(f"Generated: {filename}")
-                                config_number += 1
+                                    if config['PoolingBlock']["GlobalParams"]['Pooling'] == True:
+                                        config['PoolingBlock']['Layers'][f"Conv{layers}"]['Type'] = "Avg"
+                                        for i in range(layers + 1, 5):
+                                            config['PoolingBlock']['Layers'].pop(f"Conv{i}", None)
+                                    else:
+                                        for i in range(0, 5):
+                                            config['PoolingBlock']['Layers'].pop(f"Conv{i}", None)
+                                        
+                                    
+                                    filename = f"config{config_number}.json"
+                                    filepath = os.path.join(output_dir, filename)
+                                    
+                                    with open(filepath, 'w') as json_file:
+                                        json.dump(config, json_file, indent=4)
+                                    
+                                    print(f"Generated: {filename}")
+                                    config_number += 1
 
 # Base configuration template
 base_config = {
@@ -147,5 +151,6 @@ base_config = {
     "Rho" : 1e-3, 
     "Eta" : 0.1
 }
+
 
 generate_cnn_config_files(base_config)
